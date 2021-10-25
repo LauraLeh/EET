b698ad1ae2d0cbb3c0ba797734f327d7ac6824e5
Alina <alina.fastowski@gmail.com>
2021-10-25 17:02:52 +0200

    solution 1

---
+++ Aufgabe01/extract.py
@@ -0,0 +1,71 @@
+import sys
+import os
+from html.parser import HTMLParser
+
+
+"""Reads the contents of each html file into a string."""
+def read_file(path):
+    with open(path, "r", encoding='utf-8') as f:
+        return f.read()
+
+"""Gets the absolute filepaths of the crawled html files only.
+Each of the files' content is appended to a list, which is returned."""
+def get_files(directory_path):
+    html_files = []
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith(".html"):
+                file_path = os.path.join(root, file)
+                html_files.append(read_file(file_path))
+    return html_files
+
+
+"""The HTML Parser looks out for relevant tags. If one such start-tag 
+is encountered, its content is read into a string, until the tag closes."""
+class Parser(HTMLParser):
+
+    def __init__(self):
+        # initialize the HTMLParser class first in order to proceed
+        super().__init__()
+        self.relevant_tags = ["p", "div", "h1", "h2", "h3", "blockquote"]
+        self.is_reading_text = False
+        self.current_tag = ""
+        self.text = ""
+
+    def handle_starttag(self, tag, attrs):
+        if tag in self.relevant_tags:
+            self.current_tag = tag
+            # the "div" tag has a specific relevance constraint:
+            # consider only if it describes the article's author.
+            if tag == "div":
+                for name, value in attrs:
+                    if name == "class" and value == "authorline__author":
+                        self.is_reading_text = True
+            else:
+                self.is_reading_text = True
+
+    def handle_endtag(self, tag):
+        if tag == self.current_tag:
+            self.is_reading_text = False
+
+    def handle_data(self, data):
+        if self.is_reading_text:
+            self.text += data
+
+
+def main():
+    if len(sys.argv) != 2:
+        exit()
+
+    doc_directory = str(sys.argv[1])
+    html_files = get_files(doc_directory)
+    # initialize parser and process the html files
+    parser = Parser()
+    for file in html_files:
+        parser.feed(file)
+
+    print(" ".join(parser.text.split()))
+
+
+if __name__ == '__main__':
+    main()
---
+++ Aufgabe01/tokenize.py
@@ -0,0 +1,53 @@
+import sys
+import re
+from string import punctuation
+
+
+# punctuation that marks the end of a sentence.
+# closing brackets / quotation marks after dots are included.
+sentence_marker = ('.', '!', '?', '.)', '.]', '."')
+# accepts numbers of form 1,23 and 1.23
+float_regex = r'\d+(\.|\,)\d+'
+# considers (optional) http:// or https:// and extensions of the form "/.../...".
+# URLs are often enclosed in () brackets - this is why they're also considered here
+website_regex = r'\(*(http:\/{2}|https:\/{2})*(www\.).+\.\w{2,3}(\/\w*)*\)*'
+
+
+"""Reads extracted text and abbreviations."""
+def read_files(abbreviations_path, text_path):
+    with open(text_path, "r") as f:
+        text = f.read()
+
+    with open(abbreviations_path) as file:
+        abbr = file.readlines()
+        abbr = [line.strip() for line in abbr]
+
+    return text, abbr
+
+
+def main():
+    if len(sys.argv) != 3:
+        exit()
+
+    text, abbr = read_files(sys.argv[1], sys.argv[2])
+    sentence = ""
+
+    # split() splits the text on spaces:
+    # "some words; and some more words." -> ["some", "words;", "and", "some", "more", "words."]
+    for token in text.split():
+        # if the token is an abbreviation, a float number, a website or doesn't contain any punctuation:
+        # just add the token to the sentence as it is
+        if token in abbr or re.match(float_regex, token) or re.match(website_regex, token) or not any(
+                p in token for p in punctuation):
+            sentence += token + " "
+        # else (i.e. if the token is no special case and has some punctuation):
+        # split the token on any character that is not alpha-numeric, e.g. "Donau-Isar" -> "Donau", "-", "Isar"
+        else:
+            sentence += " ".join(re.split('(\W)', token))
+            # if the end of a potential sentence is reached: print the accumulated sentence
+            if token.endswith(sentence_marker):
+                print(sentence)
+                sentence = ""
+
+if __name__ == '__main__':
+    main()
