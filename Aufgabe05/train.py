e62c7d6946b2a4311201b6aca7523d111a8ee20c
Alina <alina.fastowski@gmail.com>
2021-11-26 11:01:22 +0100

    fix prob(class|mail) calculation

--- Aufgabe05/train.py
+++ Aufgabe05/train.py
@@ -2,11 +2,10 @@ import os
 from collections import Counter, defaultdict
 import string
 import math
+import random
 
 train_path = "dev"
-classes = [dir for dir in os.listdir(train_path) if not dir.startswith(".")]  # ignore hidden dirs like ".DS_Store"
-p_class_mail = defaultdict(lambda: defaultdict(int))
-
+classes = ["ham", "spam"]
 
 ## Extract features for the feature vector and build weight vector
 def build_feature_and_weight_vec(train_path):
@@ -24,31 +23,32 @@ def build_feature_and_weight_vec(train_path):
     features = [token[0] for token in most_common]
     weights = defaultdict(int)
     for cls in classes:
-        weights[cls] = [1 for _ in features]
+        weights[cls] = [random.randrange(-3, 3) for _ in features]
     return features, weights
 
-# calculates p(c|d) without normalization
-def calculate_non_normalized_probs(features):
-    for cls in ['ham']:
+# calculates p(c|d)
+def calculate_normalized_probs(features):
+    p_class_mail = defaultdict(int)
+
+    for cls in classes:
         class_dir = os.path.join(train_path, cls)
+        if cls == "ham":
+            p_class_mail["spam"] = math.exp(0)
+        elif cls == "spam":
+            p_class_mail["ham"] = math.exp(0)
+
         for file in os.listdir(class_dir):
             with open(os.path.join(class_dir, file), 'r', encoding="ISO-8859-1") as t:
                 text = t.read()
-                tokens = text.split()
-                feature_vec = [(" ".join([x, cls]), tokens.count(x)) for x in features]
+                document = text.split()
+                feature_vec = [(" ".join([x, cls]), document.count(x)) for x in features]
                 score = sum([weight_vec[cls][i]*feature_vec[i][1] for i in range(len(features))])
-                p_class_mail[cls][file] = math.exp(score)
+                Z = sum(p_class_mail.values())
+                p_class_mail[cls] = 1/Z * math.exp(score)
 
-# calculate p(c|d) with 1/Z factor
-def calculate_normalized_probs():
-    Z = sum(score for cls in p_class_mail for score in p_class_mail[cls].values())
-    for cls in classes:
-        for file in p_class_mail[cls]:
-            p_class_mail[cls][file] *= 1/Z
+                print(p_class_mail)
+
 
 
 features, weight_vec = build_feature_and_weight_vec(train_path)
-calculate_non_normalized_probs(features)
-calculate_normalized_probs()
-print(p_class_mail)
-
+calculate_normalized_probs(features)
