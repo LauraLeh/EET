77ec2ae0d9e414a06c23afd031c611cb0f2240ca
Laura Lehmann <laura_lehmann_@gmx.de>
2021-12-11 19:56:50 +0100

    crf-train.py änderungen der funktions struktur

--- Aufgabe06/crf-train.py
+++ Aufgabe06/crf-train.py
@@ -1,22 +1,23 @@
 from collections import defaultdict
+import itertools
 import math
 
 weights = defaultdict(float)  # weight vector
 
-
 def read_data():
     global tagset
-    tagset = set()
-    tagset.add("<s>")# all available tags
-    words, tags = ["<s>"],  ["<s>"]             # a sentence's tags (with the start-tag) # a sentence's words (with the start-token)
-    sentences = []             # list of sentences [([w,w,w,...], [t,t,t,...]), (),...]
+    tagset = {"<s>", "<\s>"}   # all available tags
+    tags = ["<s>"]             # a sentence's tags (with the start-tag)
+    words = ["<s>"]            # a sentence's words (with the start-token)
+    sentences = []             # list of sentences
+
     with open("Tiger/develop.txt") as file:
         for line in file:
             line = line.strip()
             if len(line) == 0:
                 # add end-token and end-tag
-                words.append("<s>")
-                tags.append("<s>")
+                words.append("<\s>")
+                tags.append("<\s>")
                 sentences.append((words, tags))
                 words, tags = ["<s>"], ["<s>"]
             else:
@@ -26,30 +27,15 @@ def read_data():
                 tagset.add(tag)
     return sentences
 
-
 def get_substrings(string):
-  substring_len = 3
-  string_len = len(string)
-  substrings = []
-  i = 0
-  while not i > string_len - substring_len:
-    substrings.append(string[i: i+ substring_len])
-    i += 1
+  string = " " + string + " "
+  min_range, max_range = 3, 6
+  substrings = [string[min: min+max] for min in range(len(string) - min_range) for max in range(min_range, max_range+1)]
   return substrings
 
-
-def get_features(word, tag, prev_tag):
-    features = list()
-    features.append("TT-"+prev_tag+"-"+tag) # context feature: prev_tag + tag
-    features.append("TWT-"+prev_tag+"-"+word+"-"+tag) # context feature: prev_tag + word + tag
-    features.append("TW-"+tag+"-"+word) # lexical feature: tag + word
-    if len(word) > 3: # otherwise the feature would be the same as the TW feature anyways (we're taking substrings of len 3)
-        substrings = get_substrings(word)
-        for substring in substrings:
-            features.append('TS-'+tag+"-"+substring)  # lexical feature: tag + substring (all substrings of the word)
-    # feature: tag + shape
+def get_word_shape(string):
     shape = ""
-    for x in word:
+    for x in string:
         if x.isupper():
             shape = shape + "X"
         elif x.islower():
@@ -58,63 +44,93 @@ def get_features(word, tag, prev_tag):
             shape = shape + "0"
         else:
             shape = shape + x
-    shape_old, shape_new = shape, ""
-    while shape_old != shape_new:
-        s = "".join([shape_old[x] for x in range(0, len(shape_old) - 1) if shape_old[x] != shape_old[x + 1]])
-        shape_new = shape_old
-        shape_old = s + shape_old[len(shape_old) - 1]
-    features.append("TSH-" + tag + "-" + shape_new)
+    return ''.join(x for x, _ in itertools.groupby(shape))
+
+def get_features(word, tag, prev_tag):
+    features = []
+    features.append("TT-" + prev_tag + "-" + tag) # context feature: prev_tag + tag
+    features.append("TWT-" + prev_tag + "-" + word + "-" + tag) # context feature: prev_tag + word + tag
+    features.append("TW-" + tag + "-" + word) # lexical feature: tag + word
+    features.append("TSH-" + tag + "-" + get_word_shape(word))  # lexical feature: tag + shape
+    for substring in get_substrings(word):
+        features.append('TSUB-' + tag + "-" + substring)  # lexical feature: tag + substring (all substrings of the word)
     return features
 
+def log_sum_exp(x1, x2):
+    a = max(x1, x2)
+    b = min(x1, x2)
+    return a + math.log(1 + math.exp(b-a))
 
 def local_score(features):
-    return math.exp(sum([weights[f] for f in features]))
+    return sum([weights[f] for f in features])
 
-
-def calculate_expected(words: list):
-    print(words)
-    n = len(words)
-    print(n)
-    forward = defaultdict(lambda: defaultdict(float))
-    backward = defaultdict(lambda: defaultdict(float))
-    forward[0]["<s>"] = 1
-    backward[n]["<s>"] = 1
-    # collect forward probs
+def forward(words: list):     # collect forward scores
+    n = len(words) - 1
+    forw = defaultdict(lambda: defaultdict(float))
+    forw[0]["<s>"] = 0
     for i in range(1, n):
         for tag in tagset:
-            fwd_score = 0
-            for prev_tag in forward[i-1]:
-                features = get_features(words[i], tag, prev_tag) # TT (prev_tag + tag), TSH (tag + shape), TS (tag + substring), TW (tag + word), TWT (prev_tag + word + tag)
-                fwd_score += forward[i-1][prev_tag] * local_score(features)
-            forward[i][tag] = fwd_score
-    # collect backward probs
-    for j in range(n-1, 0):
+            for prev_tag, prev_score in forw[i-1].items():
+                features = get_features(words[i], tag, prev_tag)
+                fwd_score = prev_score + local_score(features)
+                forw[i][tag] = log_sum_exp(forw[i][tag], fwd_score)
+    return forw
+
+def backward(words: list):      # collect backward scores
+    n = len(words) - 1
+    backw = defaultdict(lambda: defaultdict(float))
+    backw[n]["<\s>"] = 0
+    for j in range(n-1, 0, -1):
         for tag in tagset:
-            bwd_score = 0
-            for prev_tag in backward[j+1]:
+            for prev_tag, prev_score in backw[j+1].items():
                 features = get_features(words[j], tag, prev_tag)
-                bwd_score += backward[j+1][prev_tag] * local_score(features)
-            backward[j][tag] = bwd_score
+                bwd_score = prev_score + local_score(features)
+                backw[j][tag] = log_sum_exp(backw[j][tag], bwd_score)
+    return backw
 
-    # Gamma berechnen
+def estimated_feat_values(forward: dict, backward: dict, words: list):     # calculate estimated feature values
+    n = len(words) - 1
+    estimated = defaultdict(float)
     for i in range(1, n):
-        for tag in tagset:
-            gamma = 0
-            for prev_tag in forward[i-1]:
+        for tag, beta_score in backward[i].items():
+            for prev_tag, alpha_score in forward[i-1].items():
                 f = get_features(words[i], tag, prev_tag)
-                gamma += forward[i-1][prev_tag] * local_score(f) * backward[i][tag] / forward[n-1]['<s>']
+                gamma = alpha_score + local_score(f) + beta_score - forward[-1]["<\s>"]
+                for feature in f:
+                    estimated[feature] += 1 * gamma # the value of a feature is its occurrence - which is 1
+    return estimated
+
+def calculate_observed(words, tags):
+    observed = defaultdict(float)
+    n = len(words) - 1
+    for i in range(1, n):
+        features = get_features(words[i], tags[i], tags[i-1])
+        for feature in features:
+            observed[feature] += 1
+    return observed
 
 
-
-
-
-
-def train():
-    epochs = 1
-    sentences = read_data() # [ ([w,w,w,...],[t,t,t,...]) , ([w],[t]) ,...]
+if __name__ == "__main__":
+    epochs = 2
+    learning_rate = 0.001
+    sentences = read_data()
     for _ in range(epochs):
-        for sentence in sentences[0:1]:
-            words, tags = sentence[0], sentence[1]
-            expected = calculate_expected(words)
+        for sentence in sentences[0:5]:
+            words, tags = sentence
+            alpha = forward(words)
+            beta = backward(words)
+            efv = estimated_feat_values(alpha, beta, words)
+            ofv = calculate_observed(words, tags)
+            gradient = defaultdict(float)
+            for feature in efv:
+                gradient[feature] = ofv[feature] - efv[feature]
+            for feature in weights:
+                weights[feature] += learning_rate * gradient[feature]
+    print(weights)
 
-train()
+
+
+
+
+
+
