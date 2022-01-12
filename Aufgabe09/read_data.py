9ec87ad3df78d3034252d6ec292e238b52206186
LauraLeh <65780248+LauraLeh@users.noreply.github.com>
2022-01-12 21:27:06 +0100

    process_tree fertig build_tree attempt

--- Aufgabe09/read_data.py
+++ Aufgabe09/read_data.py
@@ -1,6 +1,5 @@
 
-# Verarbeitet den Baum und gibt u.a. die Wortliste und die Konstituentenliste zurück
-def process_tree(tree: str, s: int, index: int):
+def process_tree(tree: str, s: int, index: int): #s ist startpos
     if tree[s] != "(":
         print("Error 2 in Syntax")
         exit()
@@ -9,7 +8,7 @@ def process_tree(tree: str, s: int, index: int):
     end_index = index
     pos = s + 1
     word = False
-    while pos != len(tree):
+    while pos != len(tree): #pos != len(tree) or pos != ")"
         # pos == " " -> beginn eines Wortes
         if tree[pos] == " ":
             word = True
@@ -25,7 +24,6 @@ def process_tree(tree: str, s: int, index: int):
             w, c, e, i = process_tree(tree, pos, end_index) #return words, constituents, pos + 1
             end_index, words, constituents, pos, word = i, words+w, c+constituents, e, False
             continue
-        # pos == Teil eines Wortes, evtl Ende eines Wortes
         if word is True and tree[pos] != ")":
             current_word = current_word + tree[pos]
             if tree[pos +1] == ")":
@@ -34,21 +32,59 @@ def process_tree(tree: str, s: int, index: int):
                 end_index = index +1
             pos = pos + 1
             continue
-        # pos == ) -> Ende einer Regel, eventuell Ende des ganzen Satzes
         if tree[pos] == ")":
             constituents.append((current_const, index, end_index))
             if pos == len(tree)-1:
                 new_const = []
-                for x in range(0, len(constituents) - 1):
+                x = 0
+                while x != len(constituents):
                     c_1, c_2 = constituents[x], constituents[x + 1]
                     if c_1[1] == c_2[1] and c_1[2] == c_2[2]:
                         new_const.append((c_2[0]+"="+c_1[0], c_1[1], c_1[2]))
+                        x = x+2
                     else:
                         new_const.append(c_1)
+                        x = x+1
                 return words, new_const, pos + 1, end_index
             return words, constituents, pos + 1, end_index
 
 def build_tree(wors: list, constituentes: list):
+    # words: ['Ms.', 'Haag', 'plays', 'Elianti', '.']
+    # constituentes[('TOP=S', 0, 5), ('NP', 0, 2), ('NNP', 0, 1), ('NNP', 1, 2), ('VP', 2, 4), ('VBZ', 2, 3), ('NP', 3, 4), ('NP=NNP', 3, 4), ('.', 4, 5)]
+    sentence, max_const = [], [] # tree, max_const
+    max_distance = 0
+    marker = "$$nw$$"
+    for c in constituentes:
+        if c[2]-c[1] > max_distance: max_distance = c[2]-c[1]
+        if "=" in c[0]:
+            c_1, c_2 = c[0].split("=")
+            max_const.append((c_1 + marker, c[1], c[2]))
+            max_const.append((c_2, c[1], c[2]))
+        else:
+            max_const.append(c)
+    print(max_const)
+    d = 0
+    print(max_distance)
+    print(len(max_const))
+    while d != max_distance+1:
+        print("d ist ", d)
+        for x in range(0, len(max_const)):
+            c = max_const[x]
+            if c[2]-c[1] == 1 and d == 1:
+                w = "("+c[0]+" "+wors[c[1]]+")"
+                if w not in sentence:
+                    sentence.append(w)
+                #print(sentence)
+            elif c[2]-c[1] == d:
+                print("d ist", d, "c ist ", c)
+                sentence[c[1]] = "("+c[0]+sentence[c[1]]
+                sentence[c[2]-1] = sentence[c[2]]+")"
+            if x == len(max_const)-1:
+                d += 1
+                break
+    sentence = "".join(sentence).replace(marker, "")
+    return sentence
+
 
 with open("data/laura.txt", "r") as file:
     for line in file:
@@ -56,6 +92,8 @@ with open("data/laura.txt", "r") as file:
             w, c, _, _ = process_tree(line.replace("\n", ""), 0, 0)
             c.reverse()
             print(w, c)
+            s = build_tree(w, c)
+            print(s)
         except:
             print("Error 1 in Syntax")
             exit()
